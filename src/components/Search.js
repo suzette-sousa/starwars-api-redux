ff65f4ea2915f20210c0f910b86a417b03ea3ac9
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-06-07 00:45:30 +0200

    add func to display next page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { getQueryData } from '../store/actions/queryAction';
 import Results from './Results'
+
 
 const Search = (props) => {
   const [searchValue, setSearchValue] = useState('');
   const [chooseSearch, setChooseSearch] = useState('planets');
   const [isChecked, setIsChecked] = useState(true);
+  const [whichPage, setWhichPage] = useState(1);
+  const [newData, setNewData] = useState([]);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getQueryData(chooseSearch, searchValue));
-  }, [dispatch, chooseSearch, searchValue])
+    dispatch(getQueryData(chooseSearch, searchValue, whichPage));
+  }, [dispatch, chooseSearch, searchValue, whichPage])
   
   const {queryData} = props.queryData;
   const {loading} = props.queryData;
+  
   console.log('queries', queryData); // TODO : TO DELETE
   console.log(loading)
 
@@ -28,6 +32,20 @@ const Search = (props) => {
     setIsChecked(!isChecked);
     if(e.target.checked) e.target.checked = isChecked;
   };
+
+  const getNextPage = () => {
+    setWhichPage(whichPage + 1)
+    const datasSet = new Set([
+      ...newData,
+      ...queryData
+    ]);
+    const datasSetArr = [...datasSet];
+    console.log("set", datasSetArr)
+    setNewData(datasSetArr)
+  }
+  useEffect(() => {
+    console.log("testdatas", {newData, queryData});
+  }, [newData, queryData]);
 
   return (
     <>
@@ -61,6 +79,7 @@ const Search = (props) => {
 
       <Results {...props} />
 
+      <button onClick={getNextPage}>Page suivante</button>
     </>
   );
 };
--- src/store/actions/queryAction.js
+++ src/store/actions/queryAction.js
@@ -1,9 +1,9 @@
 import { GET_QUERYDATA, QUERYDATA_ERROR } from './actionTypes';
 import axios from 'axios';
 
-export const getQueryData = (typeSearch, wordSearch) => async dispatch => {
+export const getQueryData = (typeSearch, wordSearch, whichPage) => async dispatch => {
   let url = "https://swapi.dev/api/" + typeSearch;
-  axios.get(url, {params: {search: wordSearch}})
+  axios.get(url, {params: {search: wordSearch, page: whichPage}})
   .then(response => {
     dispatch({
       type: GET_QUERYDATA,
