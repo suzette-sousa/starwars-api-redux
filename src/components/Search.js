f781a0de5e7e66d6b3f5836b9c9696e5a9a4ac19
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-06-06 13:12:25 +0200

    create form search

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,6 +16,30 @@ const Search = (props) => {
 
   return (
     <>
+      <section>
+        <h2>Rechercher</h2>
+        <form id="searchForm">
+          <input
+            id="chooseSearch-planets"
+            type="radio"
+            name="chooseSearch"
+          />
+          <label htmlFor="chooseSearch-planets">Planète</label>
+          <input
+            id="chooseSearch-people"
+            type="radio"
+            name="chooseSearch"
+          />
+          <label htmlFor="chooseSearch-people">Personnage</label>
+          <input
+            type="search"
+            name="searchValue"
+            value=""
+            autoComplete="off"
+          />
+          <button type="submit">Rechercher</button>
+        </form>
+      </section>
     </>
   );
 };
